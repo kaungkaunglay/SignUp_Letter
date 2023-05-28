4ea05dc8500f233f2be986f4840a02aa712cbf5c
AungKhantZin <viennghink@outlook.com>
2023-05-28 19:11:20 +0630

    latest

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //Require Links 
-express = require("express");
+const express = require("express");
 const bodyParser = require('body-parser');
-request= require("request"); 
-
+const request= require("request"); 
+const https = require("https"); 
 app = express(); 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); 
@@ -11,12 +11,49 @@ app.get("/", function(req, res){
     res.sendFile(__dirname+"/signup.html");
 });
 app.post("/", function(req, res){
-  var first_name =  req.body.first_name;
-  var last_name  =  req.body.last_name;
-  var email = req.body.email;
-  res.send(first_name);
+  const first_name =  req.body.first_name;
+  const last_name  =  req.body.last_name;
+  const email = req.body.email;
+  const data = {
+    members: [
+        { 
+           email_address: email,
+           status: "subscribed",
+           merge_fields: {
+            FNAME: first_name,
+            LNAME: last_name
+           }
+        }
+    ]
+  }
+   var jsonData = JSON.stringify(data); 
+    const url = "https://us14.api.mailchimp.com/3.0/lists/89639af737";
+    const api_key = "Your api key";
+    const options = {
+        method: "POST",
+        auth: "kaungkaung:"+api_key
+    }
+    const request = https.request(url, options, function(response){
+        if(express.response.statusCode == 200){
+            res.sendFile(__dirname+"/success.html");
+        }else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+        response.on("data", function(data){
+             console.log(JSON.parse(data)); 
+        })
+    }); 
+    
+    request.write(jsonData); 
+    request.end(); 
 }); 
-app.listen(3000, function() {
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+app.listen(process.env.PORT || 3000, function() {
     console.log("Listening on port: 3000") ; 
-}) ; 
-
+}); 
+// API KEY 
+// 61fc8445869e3acbc912948d769c639e-us14
+// Audience ID
+// 89639af737
